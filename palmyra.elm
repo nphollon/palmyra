a3ecbc54c15f4825c18fe8910743c69b8e58ff61
Nick Hollon <falling.masonry@gmail.com>
2015-01-31 16:55:50 -0800

    Pendulum

--- palmyra.elm
+++ palmyra.elm
@@ -1,4 +1,6 @@
-import Graphics.Element (Element)
+import Color (..)
+import Graphics.Element (..)
+import Graphics.Collage (..)
 import Signal (..)
 import Text (plainText)
 import Mouse
@@ -8,7 +10,18 @@ main : Signal Element
 main = map display counter
 
 display : Float -> Element
-display t = plainText (toString (floor t))
+display t =
+  let
+    ticks = t * 0.1 |> floor |> toString |> plainText |> toForm
+    clockLocation = (0, 40)
+    bobLocation = (25 * (cos (0.2 * pi * t)), 3.791713 * (cos (0.4 * pi * t)) - 46.208)
+    digitalClock = group [ filled lightBlue (rect 50 20), ticks ]
+    bob = filled red (circle 10)
+    thread = traced (solid black) (segment clockLocation bobLocation)
+  in collage 120 120 [ 
+    thread,
+    digitalClock |> move (0, 50),
+    bob |> move bobLocation ]
 
 counter : Signal Float
 counter =
