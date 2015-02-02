0487b076bd7b51ba8bcb64da94a22cb9436e8fde
Nick Hollon <falling.masonry@gmail.com>
2015-02-02 12:50:22 -0800

    Express pendulum position in polar coordinates instead of cartesian

--- palmyra.elm
+++ palmyra.elm
@@ -7,30 +7,36 @@ import Mouse
 import Time
 
 main : Signal Element
-main = map display counter
+main = map (pendulum 90 (degrees 20)) counter
 
-display : Float -> Element
-display t =
+pendulum : Float -> Float -> Float -> Element
+pendulum r thetaMax t =
   let
-    ticks = t * 0.1 |> floor |> toString |> plainText |> toForm
+    theta = thetaMax * cos (0.2 * pi * t)
+    x = r * (sin <| theta)
+    y = 40 - (r * (cos <| theta))
     clockLocation = (0, 40)
-    bobLocation = (25 * (cos (0.2 * pi * t)), 3.791713 * (cos (0.4 * pi * t)) - 46.208)
-    digitalClock = group [ filled lightBlue (rect 50 20), ticks ]
+    bobLocation = (x, y)
     bob = filled red (circle 10)
     thread = traced (solid black) (segment clockLocation bobLocation)
   in collage 120 120 [ 
     thread,
-    digitalClock |> move (0, 50),
+    digitalClock t |> move clockLocation,
     bob |> move bobLocation ]
+
+digitalClock : Float -> Form
+digitalClock t =
+  let ticks = t * 0.1 |> floor |> toString |> plainText |> toForm
+  in group [ filled lightBlue (rect 50 20), ticks ] |> moveY 10
 
 counter : Signal Float
 counter =
-  let rate = \t -> Time.inMilliseconds t * 0.01
+  let rate = \t -> Time.inSeconds t * 10
   in accum rate 0 time
 
 time : Signal Time.Time
 time = let pause = toggle True Mouse.clicks
-  in Time.fpsWhen 60 pause
+  in Time.fpsWhen 100 pause
 
 toggle : Bool -> Signal a -> Signal Bool
 toggle = foldp (\_ b -> not b)
