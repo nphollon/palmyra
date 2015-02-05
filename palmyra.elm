050f9d1212e05438e39e1d87b6a8a5fdb80ba357
Nick Hollon <falling.masonry@gmail.com>
2015-02-04 20:32:26 -0800

    Encapsulate system

--- palmyra.elm
+++ palmyra.elm
@@ -1,22 +1,36 @@
 import Color (..)
 import Graphics.Element (..)
 import Graphics.Collage (..)
-import Signal (..)
+import List
+import Signal
 import Text (plainText)
 import Mouse
 import Time
 
-main : Signal Element
-main = map display counter
+type alias System = { time:Float, stocks:List Stock, flows:List Flow }
+type alias Stock = { name:String, size:Int }
+type alias Flow = { source:String, sink:String }
 
-display : Float -> Element
-display t =
+main : Signal.Signal Element
+main = Signal.map display system
+
+network = {
+    time = 0,
+    stocks = [ { name="start", size=500 }, { name="middle", size=20 }, { name="end", size=7 } ],
+    flows = [ { source="start", sink="middle" }, { source="middle", sink="end" } ]
+  }
+
+system : Signal.Signal System
+system = Signal.map (\t -> { network | time <- t }) counter
+
+
+display : System -> Element
+display sys =
   let
-    clock = collage 120 130 [ pendulum 90 (degrees 20) t |> moveY 45 ]
-    stocks = flow right [ (stock "start" 5), (spacer 10 1), (stock "end" 7) ]
-    connections = flow right [ (connection "start" "end") ]
-  in flow down [ clock, stocks, connections ]
-
+    clock = collage 120 130 [ pendulum 90 (degrees 20) sys.time |> moveY 45 ]
+    stocks = drawStocks sys.stocks
+    flows = drawFlows sys.flows
+  in flow down [ clock, stocks, flows ]
 
 pendulum : Float -> Float -> Float -> Form
 pendulum r thetaMax t =
@@ -27,35 +41,39 @@ pendulum r thetaMax t =
     thread = traced (dotted black) (segment (0, 0) (x,y))
     bob = filled red (circle 10) |> move (x,y)
 
-    ticks = t |> floor |> intElement |> toForm
+    ticks = t |> floor |> toString |> plainText |> toForm
     digitalClock = group [ filled lightBlue (rect 50 20), ticks ] |> moveY 10
   in group [ thread, digitalClock, bob ]
 
-counter : Signal Float
+drawStocks : List Stock -> Element
+drawStocks stocks = 
+  let
+    label stock = stock.name ++ "\n" ++ (toString stock.size)
+    format string = plainText string |> (container 80 40 middle) |> color lightYellow
+    draw = label >> format
+  in List.map draw stocks |> List.intersperse (spacer 10 1) |> flow right
+
+drawFlows : List Flow -> Element
+drawFlows flows =
+  let
+    label flow = "from " ++ flow.source ++ " to " ++ flow.sink
+    format string = plainText string |> (container 200 40 middle) |> color lightGreen
+    draw = label >> format
+  in List.map draw flows |> List.intersperse (spacer 10 1) |> flow right
+
+
+counter : Signal.Signal Float
 counter =
   let rate = \t -> Time.inSeconds t
   in accum rate 0 time
 
-time : Signal Time.Time
+time : Signal.Signal Time.Time
 time = let pause = toggle True Mouse.clicks
   in Time.fpsWhen 100 pause
 
-toggle : Bool -> Signal a -> Signal Bool
-toggle = foldp (\_ b -> not b)
+toggle : Bool -> Signal.Signal a -> Signal.Signal Bool
+toggle = Signal.foldp (\_ b -> not b)
 
-accum : (a -> number) -> number -> Signal a -> Signal number
-accum f = foldp (\t c -> f t + c)
+accum : (a -> number) -> number -> Signal.Signal a -> Signal.Signal number
+accum f = Signal.foldp (\t c -> f t + c)
 
-intElement : Int -> Element
-intElement = toString >> plainText
-
-
-stock : String -> Int -> Element
-stock name quantity = 
-  let label = name ++ "\n" ++ (toString quantity)
-  in plainText label |> (container 40 40 middle) |> color lightYellow
-
-connection : String -> String -> Element
-connection source sink =
-  let label = "from " ++ source ++ " to " ++ sink
-  in plainText label |> (container 120 40 middle) |> color lightGreen
