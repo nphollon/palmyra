4517e5ebd702b25b46972b6c2029c1d3a1d1b963
Nick Hollon <falling.masonry@gmail.com>
2015-02-11 21:00:31 -0800

    Extract demo main from Arc.elm into ArcExample.elm. Arc.elm now only exports one function

--- Arc.elm
+++ Arc.elm
@@ -1,24 +1,7 @@
-module Arc where
+module Arc (arc) where
 
-import Color as C
 import Graphics.Collage as GC
 import List as L
-import Mouse
-import Signal ((<~))
-
-cX = 1000
-cY = 500
-
-main = plotArc 100 (0, 0) << canvasCoords <~ Mouse.position
-
-canvasCoords : (Int, Int) -> (Float, Float)
-canvasCoords (x, y) = (toFloat x - cX/2, cY/2 - toFloat y)
-
-plotArc h head tail =
-  GC.collage cX cY [ 
-  GC.circle 5 |> GC.filled C.red |> GC.move head,
-  GC.circle 5 |> GC.filled C.blue |> GC.move tail,
-  arc 30 h tail head |> GC.traced GC.defaultLine  ]
 
 arc res h tail head =
   let
