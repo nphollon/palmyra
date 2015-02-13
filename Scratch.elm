f4f798d47a996a68ee3360be34f266c739c4929d
Nick Hollon <falling.masonry@gmail.com>
2015-02-13 12:43:19 -0800

    Refactor Scratch.elm to operate on an Array of Stocks rather than a Dict of Stocks. Now the user can specify the order!

--- Scratch.elm
+++ Scratch.elm
@@ -1,15 +1,17 @@
 import Geometry as Geo
 
+import Array as A
 import Color as C
 import Dict as D
 import Graphics.Collage as GC
+import Graphics.Element as GE
 import List as L
 import Text as T
 
 type alias Point = (Float, Float)
 
-stocks = D.fromList [(1, "Eins"), (2, "Zwei"), (3, "Drei"), (4, "Vier"), (5, "Fünf"), (6, "Sechs"), (7, "Sieben")]
-flows = [
+s = [(5, "Fünf"), (3, "Drei"), (4, "Vier"),  (6, "Sechs"), (7, "Sieben"), (2, "Zwei"), (1, "Eins")] |> A.fromList
+f = [
   (1,2), (1,6),
   (2,4), (2,5),
   (3,5), (3,7),
@@ -19,18 +21,19 @@ flows = [
   (7,2), (7,6)
   ]
 
-main = GC.collage 500 500 drawAllStocks
+main =
+  let diagram stocks = drawSystem stocks f |> GC.collage 500 500
+  in GE.flow GE.left [ diagram s, diagram (A.slice 0 -1 s) ]
 
-drawAllStocks =
+drawSystem stocks flows =
   let
-    pos = graphIds stocks
-    label = D.values stocks
-    stockForms = L.map2 drawStock (D.values pos) label
+    pos = addPositions stocks
+    stockForms = L.map drawStock (D.values pos)
     flowForms = L.map (drawFlow pos) flows
   in flowForms ++ stockForms
 
-drawStock : (Point, Point) -> String -> GC.Form
-drawStock (nodePosition, infoboxPosition) label =
+drawStock : (String, (Point, Point)) -> GC.Form
+drawStock (label, (nodePosition, infoboxPosition)) =
   let    
     info = T.plainText label |> GC.toForm
     box = GC.filled C.lightBlue (GC.rect 100 50)
@@ -40,13 +43,13 @@ drawStock (nodePosition, infoboxPosition) label =
     link = GC.traced (GC.dotted C.black) <| GC.segment nodePosition infoboxPosition
   in GC.group [ link, node , infobox ]
 
-drawFlow : D.Dict Int (Point, Point) -> (Int, Int) -> GC.Form
+drawFlow : D.Dict comparable (b, (Point, Point)) -> (comparable, comparable) -> GC.Form
 drawFlow pos flow =
   let
     tail = D.get (fst flow) pos
     head = D.get (snd flow) pos
   in case (tail, head) of
-    (Just (t,_), Just (h,_)) -> arc t h
+    (Just (_,(t,_)), Just (_,(h,_))) -> arc t h
     otherwise -> GC.group []
 
 arc : Point -> Point -> GC.Form
@@ -57,34 +60,29 @@ arc t h =
     arc = Geo.arc 5 t h |> L.filter isWithinBounds
     arrowhead = Geo.arrowhead 10 (degrees 45) (L.head arc) (L.head <| L.tail arc)
   in GC.group [
-    arc |> GC.path |> GC.traced (GC.solid C.black),
-    arrowhead |> GC.polygon |> GC.filled C.black
+      arc |> GC.path |> GC.traced (GC.solid C.black),
+      arrowhead |> GC.polygon |> GC.filled C.black
     ]
 
-graphIds : D.Dict Int a -> D.Dict Int (Point, Point)
-graphIds dict =
+addPositions : A.Array (comparable, b) -> D.Dict comparable (b, (Point, Point))
+addPositions kvPairs =
   let
-    keys = D.keys dict
-    n = L.length keys
-  in L.map2 (\k i -> (k, keyPositions n i)) keys [ 1 .. n ] |> D.fromList
+    n = A.length kvPairs
+    position i (k, v) = (k, (v, keyPositions n i))
+  in A.indexedMap position kvPairs |> A.toList |> D.fromList
 
 keyPositions : Int -> Int -> (Point, Point)
 keyPositions n i =
   let
     n' = toFloat n
     i' = toFloat i
-    angle = (i' - 0.5) / n' * degrees 360
+    angle = (i' + 0.5) / n' * degrees 360
     nodePosition = polar 100 angle
 
-    column = if i * 2 > n then -1 else 1
-    row = if i * 2 > n then n' + 1 - i' else i'
+    (row, col) = if 2 * i + 1 > n then (n' - i', 1) else (i' + 1, -1)
     rowOffset = 0.5 * toFloat (1 + ceiling (0.5 * n'))
-    infoboxPosition = (column * 180, (rowOffset - row) * 70)
+    infoboxPosition = (col * 180, (rowOffset - row) * 70)
   in (nodePosition, infoboxPosition)
 
 polar : Float -> Float -> Point
-polar r theta = 
-  let
-    x = r * (sin theta)
-    y = r * (cos theta)
-  in (x,y)
+polar r theta = Geo.polar (0,0) r (theta + degrees 90)
