a2a06eb7cbc0c9ce4c6334d163028e6bd79c800c
Nick Hollon <falling.masonry@gmail.com>
2015-03-22 23:07:31 -0700

    I have a population that works, eats, starves, and stabilizes around 100

--- Data/Agricola.elm
+++ Data/Agricola.elm
@@ -3,26 +3,40 @@ module Data.Agricola (start, turnDuration, plyPerTurn) where
 import System (..)
 import Dict
 
-turnDuration = 24.0
+turnDuration = 24
 plyPerTurn = 360
 
 start = Systemic {
   stocks = Dict.fromList [
-    ("Population", 1000),
+    ("Population", 100),
     ("Crops", 0),
-    ("Stockpile", 150),
-    ("Hunger", 0),
+    ("Stockpile", 500),
     ("Time", 0) ],
   flows = Dict.fromList [
     ("Counting", Constant "Time" (1.0/30)),
     ("Growing", Constant "Crops" 0),
-    ("Harvesting", Transfer "Crops" "Stockpile" 0.5) ],
+    ("Harvesting", Transfer "Crops" "Stockpile" 0.5),
+    ("Dying", Decay "Population" 4.186E-4 0),
+    ("Birthing", Growth "Population" 6E-4),
+    ("Eating", Constant "Stockpile" -100),
+    ("Starving", Constant "Population" 0) ],
   rules = Dict.fromList [
-    ("Seasonal", { target = "Growing", rule = (transform1 "Time" seasonal) } ) ]
+    ("Seasons Rule", { target = "Growing", rule = (transform1 "Time" seasonal) } ),
+    ("Labor Rule", { target = "Harvesting", rule = (transform1 "Population" laborious) } ),
+    ("Eating Rule", { target = "Eating", rule = (transform1 "Population" eating) } ),
+    ("Starving Rule", { target = "Dying", rule = (transform2 "Population" "Stockpile" starving) } ) ]
   }
 
 seasonal t =
   let r = floor t % 12
   in if | r <= 2 -> 0
-        | r >= 7 -> 1
-        | otherwise -> -5
+        | r >= 6 -> 6
+        | otherwise -> -500
+
+laborious p = p
+
+eating p = negate (p / 30)
+
+starving p s =
+  let deathRate = 4.186E-4
+  in if s * 30 < p then 5 * deathRate else deathRate
